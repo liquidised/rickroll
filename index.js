705369c5d25fea20956ef5a46bccd7f3fa3db70a
marcat <99297657+liquidised@users.noreply.github.com>
2023-01-01 10:05:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/verify', async (req, res) => {
+app.get('/', async (req, res) => {
 	res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 })
 
