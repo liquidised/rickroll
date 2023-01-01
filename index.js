32b6f8887b9133eb79e2bb43ef9f4980f4ab2691
marcat <99297657+liquidised@users.noreply.github.com>
2023-01-01 09:51:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/verify', async (req, res) => {
+	res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+})
+
+
+// start the server
+app.listen(port, () => {
+    console.log(`Started the server on ${port}`)
+})
